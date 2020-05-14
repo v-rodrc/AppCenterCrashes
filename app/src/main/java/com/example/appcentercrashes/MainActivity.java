33eb2644a805aad6084ba01a88f81ceaffa94d00
Christopher Rodriguez <christophererodriguez70@gmail.com>
2020-05-14 13:05:52 -0500

    initial commit

---
+++ app/src/main/java/com/example/appcentercrashes/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.appcentercrashes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "04ef84bc-42e9-4860-b7c1-9ee34ff5b943",
+                Analytics.class, Crashes.class);
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Crashes.generateTestCrash();
+            }
+        });
+    }
+}
