655010628c29865f19052cccfa9b6cf644aac085
Christopher Rodriguez <christophererodriguez70@gmail.com>
2020-05-19 12:30:24 -0500

    added pre build script to update version code

---
+++ app/appcenter-pre-build.sh
@@ -0,0 +1,68 @@
+if [ -z "$VERSION_NAME" ]
+
+			then
+
+			echo "You need define the VERSION_NAME variable in App Center"
+
+			exit
+
+			fi
+
+			ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/Droid/Properties/AndroidManifest.xml
+
+			INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/iOS/Info.plist
+
+			if [ -e "$ANDROID_MANIFEST_FILE" ]
+
+			then
+
+			echo "Updating version name to $VERSION_NAME in AndroidManifest.xml"
+
+			sed -i '' 's/versionName="[0-9.]*"/versionName="'$VERSION_NAME'"/' $ANDROID_MANIFEST_FILE
+
+			echo "File content:"
+
+			cat $ANDROID_MANIFEST_FILE
+
+
+			fi
+
+
+
+
+
+
+
+
+
+
+
+			if [ -e "$INFO_PLIST_FILE" ]
+
+
+
+			then
+
+
+
+			echo "Updating version name to $VERSION_NAME in Info.plist"
+
+
+
+			plutil -replace CFBundleShortVersionString -string $VERSION_NAME $INFO_PLIST_FILE
+
+
+
+
+
+
+
+			echo "File content:"
+
+
+
+			cat $INFO_PLIST_FILE
+
+
+
+			fi
