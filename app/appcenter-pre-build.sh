4a17f817d24d00ab5169a4f06e4f9225d3f015c9
Chris Rodriguez <v-rodrc@microsoft.com>
2020-05-27 12:55:27 -0500

    updated pre build script to update version code

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,3 +1,6 @@
+
+
+
 if [ -z "$VERSION_CODE" ]
 then
 	echo "To use this script, define VERSION_CODE in App Center build settings portal under environment variables"
@@ -6,23 +9,32 @@ fi
 
 ANDROID_BUILD_GRADLE=$APPCENTER_SOURCE_DIRECTORY/build.gradle
 
+#ANDROID_BUILD_GRADLE=/Users/christopherrodriguez/Documents/AppCenterCrashes/app/build.gradle
+
 echo "$APPCENTER_SOURCE_DIRECTORY"
 
 if [ -e "$ANDROID_BUILD_GRADLE" ]
 then
 	echo "version code:" $VERSION_CODE
 	cat $ANDROID_BUILD_GRADLE
-	echo "Updating version name to $VERSION_NAME in AndroidManifest.xml"
+	echo "Updating version name to $VERSION_CODE in build.gradle file"
 
 	# sed command is stream editor,
-	#  i\ subcommands can continue onto more than one line, provided all lines but the last end with a
-	#  \ (backslash) to quote the new-line character.
-	sed -i '' 's/versionCode "[0-9.]*"/versionCode "'$VERSION_CODE'"/' $ANDROID_BUILD_GRADLE
+	#  -i to edit files in-place instead of printing to standard output
+	#-e
+
+
+#[0-9]* matches a sequence of digits of any length
+	#sed -i '' 's/versionCode "[0-9.]*"/versionCode "'$VERSION_CODE'"/' $ANDROID_BUILD_GRADLE
+
+	sed 's/versionCode [0-9a-zA-Z -_]*/versionCode '$((2))'/' ./build.gradle
+
 	echo "File content:"
 	cat $ANDROID_BUILD_GRADLE
 
 fi
 
+#app/build.gradle
 
 
 
